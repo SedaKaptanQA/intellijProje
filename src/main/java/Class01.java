c9cb18cdf168dbe8bf14dfa0b9318f98722f210a
Seda Kaptan <sedakaptan3@gmail.com>
2023-02-24 00:03:51 +0300

    v5

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -1,6 +1,10 @@
 public class Class01 {
 
+
+
     public static void main(String[] args) {
         System.out.println("1.çalışmam");
+        System.out.println("bugün hava yağışlı");
+        System.out.println();
     }
 }
